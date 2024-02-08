4ce1e690aee7b4d081dd2a7c78bcc7bc72802945
Kevin Sinaga <kevinsinaga11.ks@gmail.com>
2024-02-08 19:33:51 +0700

    Creating CQRS Query Request and Handler - Part 2

--- HR.LeaveManagement.Application/Contracts/Persistence/IGenericRepository.cs
+++ HR.LeaveManagement.Application/Contracts/Persistence/IGenericRepository.cs
@@ -3,7 +3,7 @@
     public interface IGenericRepository<T> where T : class
     {
         Task<List<T>> GetAsync();
-        Task<T> GetByIdAsync();
+        Task<T> GetByIdAsync(int id);
         Task<T> CreateAsync(T entity);
         Task<T> UpdateAsync(T entity);
         Task<T> DeleteAsync(T entity);
